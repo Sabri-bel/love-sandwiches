64f3b912b0f19f52a5e86206b0427aebfc2b8780
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-09-03 15:57:11 +0000

    create function for collect data from user

--- run.py
+++ run.py
@@ -15,7 +15,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
-sales = SHEET.worksheet("sales")
-data = sales.get_all_values()
+def get_sales_data():
+    """
+    get sales input from user
+    """
+    print("enter sales data from the last market")
+    print("the data entered should be six numbers separated by commas")
+    print("example: 10,20,30,40,50,60\n")
 
-print(data)
+    data_str = input("Enter your data here: ")
+    print(f"the data provided is {data_str}")
+
+get_sales_data()
